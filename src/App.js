eadc5e746f2e5a36ce9711710cd6f6d274580d6e
JRGCast <joao.ralph1@gmail.com>
2021-11-09 16:19:20 -0300

    estilizando da velha maneira

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
 import { LinearProgress } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/core/styles';
 import { lazy, Suspense } from 'react';
 import NavBar from './components/NavBar';
-import theme from './styles/themeStyles';
 const Routes = lazy(() => import('./routes/Routes'));
 
 
 function App() {
   return (
-    <ThemeProvider theme={ theme }>
-      <div
-        style={ {
-          background: theme.palette.background.default,
-          height: '100%',
-          width: '100%'
-        } }>
-        <header>
-        {console.log(theme.palette)}
-          <nav>
-            <NavBar theme={theme} />
-          </nav>
-        </header>
-        <main style={ {
-         marginTop: '50em'
-        } }>
-          <section>
-            <Suspense fallback={ <LinearProgress /> }>
-              <Routes />
-            </Suspense >
-          </section>
-        </main>
-      </div>
-    </ThemeProvider>
+    <div
+      style={ {
+        height: '100%',
+        width: '100%'
+      } }>
+      <header>
+        <nav>
+          <NavBar />
+        </nav>
+      </header>
+      <main style={ {
+        marginTop: '50em'
+      } }>
+        <section>
+          <Suspense fallback={ <LinearProgress /> }>
+            <Routes />
+          </Suspense >
+        </section>
+      </main>
+    </div>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,9 @@
 import { AppBar, Toolbar } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
-import useStyles from "../styles/NavBarStyles";
 import { landingPageUrl, loginPageUrl, registerPageUrl } from "../utils/internalUrls";
+import './NavBar.css';
 
 const NavBar = () => {
-  const classes = useStyles();
   const navLinks = [landingPageUrl, loginPageUrl, registerPageUrl];
   const navLinksGen = navLinks.map((link) => {
     const linkText = link.split('/')[1].toUpperCase();
@@ -12,14 +11,13 @@ const NavBar = () => {
       <NavLink
         key={ link }
         to={ link }
-        style={ { color: 'white', textDecoration: 'none', } }
+        className='NavLink'
       >{ linkText }</NavLink>
     );
   });
   return (
-    <AppBar >
-    {console.log(classes)}
-      <Toolbar style={ { display: 'flex', justifyContent: 'space-between', margin: '0 auto', width: '50%' } }>
+    <AppBar color='secondary'>
+      <Toolbar className='NavToolBar'>
         { navLinksGen }
       </Toolbar>
     </AppBar >);
