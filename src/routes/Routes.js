fa9507e592eb1eb4764046f22cc916f6979a86aa
JRGCast <joao.ralph1@gmail.com>
2021-11-09 11:45:53 -0300

    alterado favicon, criado esqueleto das rotas e paginas iniciais

---
+++ src/routes/Routes.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route to='/'></Route>
+      </Switch>
+    </BrowserRouter>);
+};
+
+export default Routes;
