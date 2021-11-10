b9da2e6a395dedd7cd9156848c1f9598880b3b2b
JRGCast <joao.ralph1@gmail.com>
2021-11-09 23:30:03 -0300

    route do redirect (atual navigate) estava como children, nao como element

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,4 +1,4 @@
-import { Navigate, Router } from 'react-router';
+import { Navigate } from 'react-router';
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
 import LoginPage from '../pages/LoginPage';
@@ -6,15 +6,12 @@ import RegisterPage from '../pages/RegisterPage';
 import { landingPageUrl, loginPageUrl, registerPageUrl } from '../utils/internalUrls';
 const AppRoutes = () => {
   return (
-    // <Router>
-
-        <Routes>
-          <Route path={ loginPageUrl } element={ <LoginPage/> }></Route>
-          <Route path={ registerPageUrl } element={ <RegisterPage/> }></Route>
-          <Route path={ landingPageUrl } element={ <LandingPage/> }></Route>
-          {/* <Route exact path='/'>{ <Navigate to={ landingPageUrl } /> }</Route> */}
-        </Routes>
-    // </Router>
+    <Routes>
+      <Route path={ loginPageUrl } element={ <LoginPage /> }></Route>
+      <Route path={ registerPageUrl } element={ <RegisterPage /> }></Route>
+      <Route path={ landingPageUrl } element={ <LandingPage /> }></Route>
+      <Route exact path='/' element={ <Navigate to={ landingPageUrl } /> }></Route>
+    </Routes>
   );
 };
 
