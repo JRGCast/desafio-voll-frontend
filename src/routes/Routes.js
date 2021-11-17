a3dac9ab7909fca4f6d3b26f3195132287ae6f67
JRGCast <joao.ralph1@gmail.com>
2021-11-17 18:53:26 -0300

    adicionada notfoundpage nas rotas

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,14 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import ChatPage from '../pages/ChatPage';
 import LandingPage from '../pages/LandingPage';
 import LoginPage from '../pages/LoginPage';
+import NotFoundPage from '../pages/NotFoundPage';
 import { chatPageUrl, landingPageUrl, loginPageUrl } from '../utils/internalUrls';
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path={ loginPageUrl } element={ <LoginPage /> }></Route>
       <Route path={ landingPageUrl } element={ <LandingPage /> }></Route>
-      <Route path={ `${chatPageUrl}/:username` } element={  <ChatPage /> }></Route>
+      <Route path={ `${chatPageUrl}/:username` } element={ <ChatPage /> }></Route>
       <Route exact path='/' element={ <Navigate to={ landingPageUrl } /> }></Route>
+      <Route path='*' element={ <NotFoundPage /> } />
     </Routes>
   );
 };
